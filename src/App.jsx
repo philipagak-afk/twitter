fafb29efdd3a1ec5e55fc3322ba388220bacd1d3
philipagak <philipagak@gmail.com>
2026-09-24 16:54:33 -0400

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,62 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router";
+
 import Layout from "./pages/Layout";
+import ProtectedLayout from "./pages/ProtectedLayout";
+
 import Home from "./pages/Home";
+import Messages from "./pages/Messages";
+import Profile from "./pages/Profile";
+import Signout from "./pages/Signout";
+
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+
+        {/* PUBLIC PAGES */}
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/signin" element={<Signin />} />
+
+
+        {/* PROTECTED PAGES */}
+        <Route element={<ProtectedLayout />}>
+
+          <Route element={<Layout />}>
+
+            <Route path="/" element={<Home />} />
+
+            <Route
+              path="/messages"
+              element={<Messages />}
+            />
+
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+
+            <Route
+              path="/signout"
+              element={<Signout />}
+            />
+
+          </Route>
+
         </Route>
 
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
