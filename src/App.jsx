5a58b1a2191a3105c128e5cbd03e1a0fd6952529
philipagak <philipagak@gmail.com>
2026-09-26 16:08:15 -0400

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router";
 
 import Layout from "./pages/Layout";
 import ProtectedLayout from "./pages/ProtectedLayout";
 
 import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import Notification from "./pages/Notifications";
 import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
 import Signout from "./pages/Signout";
@@ -12,34 +19,68 @@ import Signout from "./pages/Signout";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 
-import Notification from "./pages/Notification";
-import Explore from "./pages/Explore";
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* Public pages */}
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+        {/* PUBLIC ROUTES */}
 
-        {/* Protected pages */}
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+
+        <Route
+          path="/signin"
+          element={<Signin />}
+        />
+
+
+        {/* PROTECTED ROUTES */}
+
         <Route element={<ProtectedLayout />}>
+
           <Route element={<Layout />}>
 
-            <Route path="/" element={<Home />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/signout" element={<Signout />} />
-            <Route path="/explore" element={<Explore />} />
+            {/* HOME */}
+            <Route
+              path="/"
+              element={<Home />}
+            />
 
-<Route
-  path="/notifications"
-  element={<Notification />}
-/>
+            {/* EXPLORE */}
+            <Route
+              path="/explore"
+              element={<Explore />}
+            />
+
+            {/* NOTIFICATIONS */}
+            <Route
+              path="/notifications"
+              element={<Notification />}
+            />
+
+            {/* MESSAGES */}
+            <Route
+              path="/messages"
+              element={<Messages />}
+            />
+
+            {/* PROFILE */}
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+
+            {/* SIGN OUT */}
+            <Route
+              path="/signout"
+              element={<Signout />}
+            />
 
           </Route>
+
         </Route>
 
       </Routes>
