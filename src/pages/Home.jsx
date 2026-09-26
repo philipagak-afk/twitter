a0edeb65ef8bc853ccf8386aea750ca3d9684c63
philipagak <philipagak@gmail.com>
2026-09-26 16:45:53 -0400

    update project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,7 @@ function Home() {
       <div className="feed">
 
         <div className="feed-message">
-          Error fetching Tweets
+          
         </div>
 
       </div>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,4 +1,12 @@
 import { NavLink, Outlet } from "react-router";
+import {
+  FaHome,
+  FaSearch,
+  FaBell,
+  FaEnvelope,
+  FaUser,
+  FaSignOutAlt,
+} from "react-icons/fa";
 
 function Layout() {
   return (
@@ -11,72 +19,94 @@ function Layout() {
 
         <nav className="sidebar-nav">
 
+          {/* HOME */}
           <NavLink
-          o="/explore"
-  className={({ isActive }) =>
-    isActive ? "nav-item active" : "nav-item"
-  }
->
-  <span className="nav-icon">⌕</span>
-  <span>Explore</span>
-</NavLink>
-
-<NavLink
-  to="/notifications"
-  className={({ isActive }) =>
-    isActive ? "nav-item active" : "nav-item"
-  }
->
-  <span className="nav-icon">♡</span>
-  <span>Notifications</span>
             to="/"
             className={({ isActive }) =>
               isActive ? "nav-item active" : "nav-item"
             }
-          
-            <span className="nav-icon">⌂</span>
+          >
+            <span className="nav-icon">
+              <FaHome />
+            </span>
             <span>Home</span>
           </NavLink>
 
+          {/* EXPLORE */}
+          <NavLink
+            to="/explore"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            <span className="nav-icon">
+              <FaSearch />
+            </span>
+            <span>Explore</span>
+          </NavLink>
+
+          {/* NOTIFICATIONS */}
+          <NavLink
+            to="/notifications"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            <span className="nav-icon">
+              <FaBell />
+            </span>
+            <span>Notifications</span>
+          </NavLink>
+
+          {/* MESSAGES */}
           <NavLink
             to="/messages"
             className={({ isActive }) =>
               isActive ? "nav-item active" : "nav-item"
             }
           >
-            <span className="nav-icon">✉</span>
+            <span className="nav-icon">
+              <FaEnvelope />
+            </span>
             <span>Messages</span>
           </NavLink>
 
+          {/* PROFILE */}
           <NavLink
             to="/profile"
             className={({ isActive }) =>
               isActive ? "nav-item active" : "nav-item"
             }
           >
-            <span className="nav-icon">♙</span>
+            <span className="nav-icon">
+              <FaUser />
+            </span>
             <span>Profile</span>
           </NavLink>
 
+          {/* LOG OUT */}
           <NavLink
             to="/signout"
             className="logout-button"
           >
-            Log Out
+            <FaSignOutAlt />
+            <span>Log Out</span>
           </NavLink>
 
         </nav>
 
         {/* USER INFORMATION */}
         <div className="sidebar-user">
+
           <div className="avatar">
-            👤
+            <FaUser />
           </div>
 
           <div>
             <h4>test one</h4>
             <p>@test_one</p>
           </div>
+
         </div>
 
       </aside>
@@ -91,3 +121,4 @@ function Layout() {
 }
 
 export default Layout;
+
